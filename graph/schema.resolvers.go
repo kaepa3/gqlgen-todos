444d573c81a1c52c1dc5aec8e71816b1c5ab41e6
kaepa3 <kaepa3@gmail.com>
2020-03-01 08:44:50 +0000

    add source

---
+++ graph/schema.resolvers.go
@@ -0,0 +1,32 @@
+// This file will be automatically regenerated based on the schema, any resolver implementations
+// will be copied through when generating and any unknown code will be moved to the end.
+package graph
+
+import (
+	"context"
+	"fmt"
+	"math/rand"
+
+	"github.com/kaepa3/gqlgen-todos/graph/generated"
+	"github.com/kaepa3/gqlgen-todos/graph/model"
+)
+
+func (r *mutationResolver) CreateTodo(ctx context.Context, input model.NewTodo) (*model.Todo, error) {
+	todo := &model.Todo{
+		Text: input.Text,
+		ID:   fmt.Sprintf("T%d", rand.Int()),
+		User: &model.User{ID: input.UserID, Name: "user " + input.UserID},
+	}
+	r.todos = append(r.todos, todo)
+	return todo, nil
+}
+
+func (r *queryResolver) Todos(ctx context.Context) ([]*model.Todo, error) {
+	return r.todos, nil
+}
+
+func (r *Resolver) Mutation() generated.MutationResolver { return &mutationResolver{r} }
+func (r *Resolver) Query() generated.QueryResolver       { return &queryResolver{r} }
+
+type mutationResolver struct{ *Resolver }
+type queryResolver struct{ *Resolver }
